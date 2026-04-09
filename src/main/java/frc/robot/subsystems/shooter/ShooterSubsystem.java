cfc8d27ac7d8a877ab7c75c1c8aa61bf2340f29b
TJCyan <47479486+tjin2019@users.noreply.github.com>
2026-04-09 19:28:11 -0400

    Update ShooterSubsystem.java

--- src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -2,23 +2,23 @@ package frc.robot.subsystems.shooter;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.util.SmartKrakenMotor;
+import frc.robot.util.SmartTalonMotor;
 
 public class ShooterSubsystem extends SubsystemBase {
     
-    public final SmartKrakenMotor fatKickerInnerMotor;
-    public final SmartKrakenMotor fatKickerOuterMotor;
+    public final SmartTalonMotor fatKickerInnerMotor;
+    public final SmartTalonMotor fatKickerOuterMotor;
 
     //[TODO]Set correct values for PID, output & angle range, gear ratio, motor mode for all motors
     public ShooterSubsystem() {
-        this.fatKickerInnerMotor = SmartKrakenMotor.Builder.newInstance()
+        this.fatKickerInnerMotor = SmartTalonMotor.Builder.newInstance()
             .port(Constants.Shooter.FAT_KICKER_INNER_MOTOR_PORT)
             .PID(1,0,0)
             .outputRange(-1,-1)
-            .mode(SmartKrakenMotor.MotorMode.CONTINUOUS)
+            .mode(SmartTalonMotor.MotorMode.CONTINUOUS)
             //.gearRatio()
             .build();
-        this.fatKickerOuterMotor = SmartKrakenMotor.Builder.newInstance()
+        this.fatKickerOuterMotor = SmartTalonMotor.Builder.newInstance()
             .port(Constants.Shooter.FAT_KICKER_OUTER_MOTOR_PORT)
             .PID(1,0,0)
             .outputRange(-1,-1)
