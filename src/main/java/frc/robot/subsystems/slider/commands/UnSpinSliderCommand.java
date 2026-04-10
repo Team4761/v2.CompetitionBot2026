ff3778161f4a4952760e2821c705abd7511f1c74
kugelblitz150-hash <kugelblitz150@gmail.com>
2026-04-10 19:38:27 -0400

    add un commands for both the slider and kicker

---
+++ src/main/java/frc/robot/subsystems/slider/commands/UnSpinSliderCommand.java
@@ -0,0 +1,32 @@
+package frc.robot.subsystems.slider.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.slider.SliderSubsystem;
+
+public class UnSpinSliderCommand extends Command{
+    private final SliderSubsystem sliderSubsystem;
+    public UnSpinSliderCommand(SliderSubsystem sliderSubsystem) {
+        this.sliderSubsystem = sliderSubsystem;
+        addRequirements(sliderSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+        sliderSubsystem.sliderMotor.setSpeed(-1 * Constants.Slider.SLIDER_RPM);
+    }
+    
+    public void execute() {
+        // No need to set the motor speed again since it's already set in initialize() and this command is meant to run until interrupted
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        sliderSubsystem.sliderMotor.stopTurning();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false; // This command will run until interrupted
+    }
+}
