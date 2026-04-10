9bcbf9c680207cb879cbaa39614b4c4ee721f4d9
TJCyan <47479486+tjin2019@users.noreply.github.com>
2026-04-09 20:48:18 -0400

    moved Slider Command to commands folder

--- src/main/java/frc/robot/commands/slider/SpinSliderCommand.java
+++ src/main/java/frc/robot/commands/slider/SpinSliderCommand.java
@@ -1,7 +1,8 @@
-package frc.robot.subsystems.slider;
+package frc.robot.commands.slider;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
+import frc.robot.subsystems.slider.SliderSubsystem;
 
 public class SpinSliderCommand extends Command{
     private final SliderSubsystem sliderSubsystem;
