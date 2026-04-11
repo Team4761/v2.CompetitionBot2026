85119bafaeb18f9ca727b521b1668156bac7ad10
kugelblitz150-hash <kugelblitz150@gmail.com>
2026-04-11 11:40:00 -0400

    updated shoot command to be better
    changed it to a parrelel command group that starts the slider and shooter at the same time but waits to start the kicker

--- src/main/java/frc/robot/subsystems/shooter/commands/ShootCommand.java
+++ src/main/java/frc/robot/subsystems/shooter/commands/ShootCommand.java
@@ -1,22 +1,21 @@
 package frc.robot.subsystems.shooter.commands;
 
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.Constants.Slider;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants.Shooter.ShootConfig;
 import frc.robot.subsystems.kicker.KickerSubsystem;
 import frc.robot.subsystems.kicker.commands.KickCommand;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
 import frc.robot.subsystems.slider.SliderSubsystem;
 import frc.robot.subsystems.slider.commands.SpinSliderCommand;
 
-public class ShootCommand extends SequentialCommandGroup{
-    private KickerSubsystem kickerSubsystem;
-    private SliderSubsystem sliderSubsystem;
-    private ShooterSubsystem shooterSubsystem;
+public class ShootCommand extends ParallelCommandGroup{
 
     public ShootCommand(KickerSubsystem subKick, SliderSubsystem subSlide, ShooterSubsystem subShoot) {
         addCommands(
             new SpinSliderCommand(subSlide),
-            new KickCommand(subKick),
+            new WaitCommand(ShootConfig.KICKER_INIT_DELAY).andThen(new KickCommand(subKick)),
             new RunSpitterCommand(subShoot)
         );
     }
