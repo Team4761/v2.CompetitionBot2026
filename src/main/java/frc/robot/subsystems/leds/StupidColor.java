9e23f42b2838d9ea96781af67aac3742f1257c9e
TheManBehindTheMemes <alex@merc-law.com>
2026-04-11 12:55:39 -0400

    Added StupidColor
    addedf the stupid color class from last year, incase we are using GRB LEDs again. If we aren't, i will still use it to reduce the brightness of LEDs

---
+++ src/main/java/frc/robot/subsystems/leds/StupidColor.java
@@ -0,0 +1,51 @@
+package frc.robot.subsystems.leds;
+
+import edu.wpi.first.wpilibj.util.Color;
+
+/**
+ * Accounts for if we are
+ */
+public class StupidColor extends Color {
+
+    private static double ledBrightness = 0.5;
+
+    /**
+     * Just use this like a normal Color, but when the code is sent off the to bot, the R & G channels will be switched.
+     * RGB -> GRB
+     * @param R 0-255
+     * @param G 0-255
+     * @param B 0-255
+     */
+    public StupidColor(int R, int G, int B) {
+        this(R / 255.0, G / 255.0, B / 255.0);
+    }
+
+
+    /**
+     * Just use this like a normal Color, but when the code is sent off the to bot, the R & G channels will be switched.
+     * RGB -> GRB
+     * @param R
+     * @param G
+     * @param B
+     */
+    private StupidColor(double R, double G, double B) {
+        super(G*ledBrightness, R*ledBrightness, B*ledBrightness);
+    }
+
+    public static StupidColor stupidifyColor(Color color) {
+        return new StupidColor(color);
+    }
+    /**
+     * Just use this like a normal Color, but when the code is sent off the to bot, the R & G channels will be switched.
+     * RGB -> GRB
+     * @param color
+     */
+    
+    public StupidColor(Color color) {
+        this(color.red, color.green, color.blue);
+    }
+
+
+    public static double getLEDBrightness() { return ledBrightness; }
+    public static void setLEDBrightness(double ledBrightness) { StupidColor.ledBrightness = ledBrightness; }
+}
