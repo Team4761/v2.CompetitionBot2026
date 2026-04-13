54d057102286b89f6186923c93e0b28f470ec856
Benjamin Clark <benclarkbobb@gmail.com>
2026-04-12 21:20:40 -0400

    Fixes

--- src/main/java/frc/robot/commandGroups/ShootWithPowerCommand.java
+++ src/main/java/frc/robot/commandGroups/ShootWithPowerCommand.java
@@ -16,12 +16,12 @@ import frc.robot.subsystems.slider.SliderSubsystem;
 public class ShootWithPowerCommand extends SequentialCommandGroup {
     public ShootWithPowerCommand(ShooterSubsystem shooter, SliderSubsystem slider, KickerSubsystem kicker, DoubleSupplier powerSupplier) {
         super(
-            new InstantCommand(() -> shooter.spitterMotor.setSpeedPercent(-0.05)),
+            new InstantCommand(() -> shooter.spitterMotor.setSpeedPercent(powerSupplier.getAsDouble())), // [TODO] Change this to a real command that sets the spitter motors to the correct speed based on the supplied power
             //new WaitCommand(0.5), // [TODO] Remove this and make the above command into a real command that times out when its up to speed
-            new InstantCommand(() -> shooter.backspinMotor.setSpeedPercent(-0.1)),
-            new InstantCommand(() -> kicker.fatKickerInnerMotor.setSpeedPercent(-0.1)),
-            new InstantCommand(() -> kicker.fatKickerOuterMotor.setSpeedPercent(-0.1)),
-            new InstantCommand(() -> slider.sliderMotor.setSpeedPercent(0.1))
+            new InstantCommand(() -> shooter.backspinMotor.setSpeed(-1000)),
+            new InstantCommand(() -> kicker.fatKickerInnerMotor.setSpeedPercent(-1000)),
+            new InstantCommand(() -> kicker.fatKickerOuterMotor.setSpeedPercent(-1000)),
+            new InstantCommand(() -> slider.sliderMotor.setSpeedPercent(1000))
         );
     }
 }
