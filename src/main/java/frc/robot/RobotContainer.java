5b5c998cb6f120d8686f9bd2a6aa8b269da99d41
kugelblitz150-hash <kugelblitz150@gmail.com>
2026-04-13 20:51:26 -0400

    Registered the commands i used for the autos in configure path planers autos

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -41,6 +41,7 @@ import frc.robot.subsystems.swerve.CommandSwerveDrivetrain;
 import frc.robot.subsystems.swerve.commands.AutoOrientCommand;
 import frc.robot.subsystems.vision.VisionSubsystem;
 import frc.robot.subsystems.whirligig.WhirligigSubsystem;
+import frc.robot.commandGroups.*;
 
 public class RobotContainer {
     public final SnatcherSubsystem snatcher = new SnatcherSubsystem();
@@ -80,7 +81,9 @@ public class RobotContainer {
     }
 
     private void configurePathPlannerAutos() {
-        NamedCommands.registerCommand("Do Nothing", Commands.none());
+        NamedCommands.registerCommand("DoNothingCommand", Commands.none());
+        NamedCommands.registerCommand("AutoShootCommand", new AutoShootCommand(drivetrain,shooter,slider,kicker));
+        NamedCommands.registerCommand("SnatchCommand", new SnatchCommand(snatcher));
         AutoBuilder.buildAutoChooser(); // [TODO] Pass in actual auto paths and event map once they are created
     }
 
